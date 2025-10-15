c707d526dd2806ef91ac0654dabd967bcc5baece
Tommy <thughes@saxtechnology.com>
2025-10-15 16:46:21 -0400

    Add placeholder js/admin-analytics.js to satisfy admin.html includes and prevent 404s

---
+++ js/admin-analytics.js
@@ -0,0 +1,8 @@
+// Admin analytics placeholder to prevent 404 and attach stubs used by admin.html
+console.log('[Admin] admin-analytics.js loaded');
+
+window.generateFullRAGReport = window.generateFullRAGReport || function(){ console.log('[Admin] generateFullRAGReport stub'); };
+window.updateAnalysisTimeframe = window.updateAnalysisTimeframe || function(){ console.log('[Admin] updateAnalysisTimeframe stub'); };
+window.showConversationDrilldown = window.showConversationDrilldown || function(){ console.log('[Admin] showConversationDrilldown stub'); };
+window.showUserDrilldown = window.showUserDrilldown || function(){ console.log('[Admin] showUserDrilldown stub'); };
+window.showSystemHealthDrilldown = window.showSystemHealthDrilldown || function(){ console.log('[Admin] showSystemHealthDrilldown stub'); };
